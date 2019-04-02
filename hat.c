7ec5402584255eda7f3b6e8cf43e831008472c3f
Mayoorasm <mayoorasm1998@gmail.com>
2019-04-02 15:55:52 +0530

    second commit

--- hat.c
+++ hat.c
@@ -1,6 +1,20 @@
 #include<stdio.h>
 void main()
 {
-printf("your voice is calm");
+int f1,f2,next,n,i;
+printf("enter the limit for fibnocii");
+scanf("%d",&n);
+f1=0;
+f2=1;
+printf("%d%d",f1,f2);
+for(i=1;i<=n;i++)
+{
+next=f1+f2;
+printf("%d",next);
+f1=f2;
+f2=next;
 }
+
+}
+
 
